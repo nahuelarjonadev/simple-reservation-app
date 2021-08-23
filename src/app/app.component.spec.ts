5392d3d935264e9e07a31b7c55ca4f1b657fc5a4
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2021-08-22 23:42:07 -0300

    fix tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,10 @@
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+
+@Component({ selector: 'app-header', template: '' })
+class HeaderStubComponent {}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -20,12 +24,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('reservations');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('reservations app is running!');
   });
 });
