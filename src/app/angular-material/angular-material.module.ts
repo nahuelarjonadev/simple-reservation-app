07252f9ece60e47c36e802e137ba2f06f4ee2357
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2021-08-23 02:46:01 -0300

    import mat-select

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  exports: [MatToolbarModule],
+  exports: [MatToolbarModule, MatSelectModule],
 })
 export class AngularMaterialModule {}
