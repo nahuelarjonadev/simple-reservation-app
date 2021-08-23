1a9043ef6f8611ec4f34edd7532eb471a93921a7
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2021-08-23 20:16:12 -0300

    add material imports for date-selector

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSelectModule } from '@angular/material/select';
-import { MatOptionModule } from '@angular/material/core';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  exports: [MatToolbarModule, MatSelectModule, MatOptionModule, MatDatepickerModule],
+  exports: [
+    MatToolbarModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+  ],
 })
 export class AngularMaterialModule {}
