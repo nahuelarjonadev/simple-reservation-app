b394573223360f38a82411087be9cbfceb4e11c2
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2021-08-23 01:10:14 -0300

    add RoomSelector to the empty base route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoomSelectorComponent } from './make-reservation/room-selector/room-selector.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: RoomSelectorComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
