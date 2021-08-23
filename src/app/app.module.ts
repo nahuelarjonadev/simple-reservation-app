27ba3d15ffffbd161d685e7b00d37bf7519ebc00
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2021-08-22 23:06:36 -0300

    sort imports in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppHeaderComponent } from './header/header.component';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [AppComponent, AppHeaderComponent],
