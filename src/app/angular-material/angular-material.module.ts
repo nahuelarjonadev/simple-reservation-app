52076ab61ccb66cb9d0d34254c8fc56de6b92ada
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2021-08-23 04:29:32 -0300

    import MatDatePickerModule

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @NgModule({
-  exports: [MatToolbarModule, MatSelectModule, MatOptionModule],
+  exports: [MatToolbarModule, MatSelectModule, MatOptionModule, MatDatepickerModule],
 })
 export class AngularMaterialModule {}
