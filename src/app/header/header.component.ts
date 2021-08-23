788aaea1c54d7aa62078f8bb2a8d0d797e93d05a
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2021-08-23 00:32:16 -0300

    add return statement to appease eslint gods

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,5 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppHeaderComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    return;
+  }
 }
