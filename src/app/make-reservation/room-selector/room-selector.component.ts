b23a3c87683a346f7a66e553444146fac0430991
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2021-08-23 01:06:54 -0300

    create room-selector component

---
+++ src/app/make-reservation/room-selector/room-selector.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-room-selector',
+  templateUrl: './room-selector.component.html',
+  styleUrls: ['./room-selector.component.sass'],
+})
+export class RoomSelectorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    return;
+  }
+}
