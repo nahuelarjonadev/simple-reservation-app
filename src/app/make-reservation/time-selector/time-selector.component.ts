517e5e5a9e3dcbb00345668818d01ae8f67ea111
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2021-08-24 13:04:57 -0300

    implement a basic time selector

--- src/app/make-reservation/time-selector/time-selector.component.ts
+++ src/app/make-reservation/time-selector/time-selector.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-time-selector',
   templateUrl: './time-selector.component.html',
-  styleUrls: ['./time-selector.component.sass']
+  styleUrls: ['./time-selector.component.sass'],
 })
 export class TimeSelectorComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  public timeSlots: string[] = [
+    '9:00',
+    '9:30',
+    '10:00',
+    '10:30',
+    '11:00',
+    '11:30',
+    '12:00',
+    '12:30',
+    '16:00',
+    '16:30',
+    '17:00',
+    '17:30',
+  ];
 
   ngOnInit(): void {
+    return;
   }
-
 }
