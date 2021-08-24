d7340e01e60273c98abf78c7729c727b69dd292f
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2021-08-24 14:55:54 -0300

    replace timeSlots field with the usage of generateBaseTimeSlots

--- src/app/make-reservation/time-selector/time-selector.component.ts
+++ src/app/make-reservation/time-selector/time-selector.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as moment from 'moment';
 import { MakeReservationService } from '../make-reservation.service';
 
 @Component({
@@ -10,33 +9,18 @@ import { MakeReservationService } from '../make-reservation.service';
 export class TimeSelectorComponent implements OnInit {
   constructor(private makeReservationService: MakeReservationService) {}
 
-  public timeSlots: string[] = [];
-  private availableTimeSlots: string[] = [];
-
-  generateTimeSlots() {
-    const generateBetweenTimes = (startTime: moment.Moment, endTime: moment.Moment) => {
-      while (startTime <= endTime) {
-        const newTime: string = moment(startTime).format('HH:mm');
-        this.timeSlots.push(newTime);
-        startTime.add(30, 'minutes');
-      }
-    };
-
-    let startTime: moment.Moment = moment().utc().set({ hour: 9, minute: 0 });
-    let endTime: moment.Moment = moment().utc().set({ hour: 12, minute: 30 });
-    generateBetweenTimes(startTime, endTime);
-
-    startTime.set({ hour: 16, minute: 0 });
-    endTime.set({ hour: 19, minute: 30 });
-    generateBetweenTimes(startTime, endTime);
+  getTimeSlots(): string[] {
+    return this.makeReservationService.generateBaseTimeSlots();
   }
+
+  // cache this locally for performance
+  private availableTimeSlots: string[] = [];
 
   isTimeAvailable(time: string): boolean {
     return this.availableTimeSlots.includes(time);
   }
 
   ngOnInit(): void {
-    this.generateTimeSlots();
     this.availableTimeSlots = this.makeReservationService.getTimeSlotsForCurrentSelection();
 
     this.makeReservationService.getOnRoomSelectedListener().subscribe(() => {
