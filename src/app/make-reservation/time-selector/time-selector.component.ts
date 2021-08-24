c9f5a221e9a3a9176af1d5923793a5b1e7b9becb
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2021-08-24 17:00:10 -0300

    hook the time selector to the service

--- src/app/make-reservation/time-selector/time-selector.component.ts
+++ src/app/make-reservation/time-selector/time-selector.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSelectionList, MatSelectionListChange } from '@angular/material/list';
 import { MakeReservationService } from '../make-reservation.service';
 
 @Component({
@@ -9,9 +10,8 @@ import { MakeReservationService } from '../make-reservation.service';
 export class TimeSelectorComponent implements OnInit {
   constructor(private makeReservationService: MakeReservationService) {}
 
-  getTimeSlots(): string[] {
-    return this.makeReservationService.generateBaseTimeSlots();
-  }
+  @ViewChild(MatSelectionList)
+  matList!: MatSelectionList;
 
   // cache this locally for performance
   private availableTimeSlots: string[] = [];
@@ -20,15 +20,38 @@ export class TimeSelectorComponent implements OnInit {
     return this.availableTimeSlots.includes(time);
   }
 
+  getTimeSlots(): string[] {
+    return this.makeReservationService.generateBaseTimeSlots();
+  }
+
+  setTimeSlot(change: MatSelectionListChange) {
+    this.makeReservationService.setSelectedTimeSlot(change.options[0]?.value);
+  }
+
   ngOnInit(): void {
     this.availableTimeSlots = this.makeReservationService.getTimeSlotsForCurrentSelection();
 
     this.makeReservationService.getOnRoomSelectedListener().subscribe(() => {
       this.availableTimeSlots = this.makeReservationService.getTimeSlotsForCurrentSelection();
+      this.makeReservationService.setSelectedTimeSlot('');
     });
 
     this.makeReservationService.getOnDateSelectedListener().subscribe(() => {
       this.availableTimeSlots = this.makeReservationService.getTimeSlotsForCurrentSelection();
+      this.makeReservationService.setSelectedTimeSlot('');
+    });
+
+    this.makeReservationService.getOnTimeSlotSelectedListener().subscribe((timeSlot: string) => {
+      this.matList.deselectAll();
+      this.matList.options.forEach((...[option]) => {
+        if (option.value === timeSlot && !option.selected) option.toggle();
+      });
+
+      const a = this.makeReservationService;
+
+      console.log(
+        'final selection: ' + a.getSelectedRoom() + ' ' + a.getSelectedDate() + ' ' + a.getSelectedTimeSlot()
+      );
     });
   }
 }
