ffa3668a531fb8fa63a8d349bb11624017af7564
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2021-08-24 14:38:26 -0300

    make date selector stateless by working with state from service

--- src/app/make-reservation/date-selector/date-selector.component.ts
+++ src/app/make-reservation/date-selector/date-selector.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { MatCalendar } from '@angular/material/datepicker';
 
 import { MakeReservationService } from '../make-reservation.service';
-import { RoomDates } from '../room-dates.model';
 
 @Component({
   selector: 'app-date-selector',
@@ -13,29 +12,29 @@ import { RoomDates } from '../room-dates.model';
 export class DateSelectorComponent implements OnInit {
   constructor(private makeReservationService: MakeReservationService) {}
 
-  private currentRoomDates: RoomDates = {};
-  public selectedDate!: Date | null;
-
   @ViewChild(MatCalendar)
   calendar!: MatCalendar<Date>;
 
   ngOnInit(): void {
-    const selectedRoom = this.makeReservationService.getSelectedRoom();
-    this.currentRoomDates = this.makeReservationService.getAvailableDatesForRoom(selectedRoom);
-
-    this.makeReservationService.getOnRoomSelectedListener().subscribe((room: string) => {
-      this.currentRoomDates = this.makeReservationService.getAvailableDatesForRoom(room);
-      console.log(this.currentRoomDates);
+    this.makeReservationService.getOnRoomSelectedListener().subscribe(() => {
       this.calendar.updateTodaysDate();
     });
+
+    this.makeReservationService.getOnDateSelectedListener().subscribe((date: Date) => {
+      this.calendar.selected = date;
+    });
+  }
+
+  setSelectedDate(date: Date | null) {
+    if (!date) return;
+
+    this.makeReservationService.setSelectedDate(date);
   }
 
   getAvailableDatesForRoom = (d: Date | null): boolean => {
     const date = d || new Date();
-    const year = this.currentRoomDates[date.getFullYear()];
-    const month = year ? year[date.getMonth() + 1] : false;
-    const day = month ? month[date.getDate()] : false;
+    const timeSlots = this.makeReservationService.getTimeSlots(this.makeReservationService.getSelectedRoom(), date);
 
-    return day !== false && day !== undefined;
+    return timeSlots.length > 0;
   };
 }
