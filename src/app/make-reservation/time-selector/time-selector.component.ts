4bb85725d52a406c210d36a3e598e05416ac1112
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2021-08-24 13:14:01 -0300

    dynamically generate time slots

--- src/app/make-reservation/time-selector/time-selector.component.ts
+++ src/app/make-reservation/time-selector/time-selector.component.ts
@@ -9,22 +9,27 @@ import * as moment from 'moment';
 export class TimeSelectorComponent implements OnInit {
   constructor() {}
 
-  public timeSlots: string[] = [
-    '9:00',
-    '9:30',
-    '10:00',
-    '10:30',
-    '11:00',
-    '11:30',
-    '12:00',
-    '12:30',
-    '16:00',
-    '16:30',
-    '17:00',
-    '17:30',
-  ];
+  public timeSlots: string[] = [];
+
+  generateTimeSlots() {
+    const generateBetweenTimes = (startTime: moment.Moment, endTime: moment.Moment) => {
+      while (startTime <= endTime) {
+        const newTime: string = moment(startTime).format('HH:mm');
+        this.timeSlots.push(newTime);
+        startTime.add(30, 'minutes');
+      }
+    };
+
+    let startTime: moment.Moment = moment().utc().set({ hour: 9, minute: 0 });
+    let endTime: moment.Moment = moment().utc().set({ hour: 12, minute: 30 });
+    generateBetweenTimes(startTime, endTime);
+
+    startTime.set({ hour: 16, minute: 0 });
+    endTime.set({ hour: 19, minute: 30 });
+    generateBetweenTimes(startTime, endTime);
+  }
 
   ngOnInit(): void {
-    return;
+    this.generateTimeSlots();
   }
 }
