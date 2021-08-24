64d7165a2e7a82b7ee8a31860e0056c7c1a98ac1
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2021-08-23 23:04:01 -0300

    filter available dates in the date picker

--- src/app/make-reservation/date-selector/date-selector.component.ts
+++ src/app/make-reservation/date-selector/date-selector.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MakeReservationService } from '../make-reservation.service';
+import { RoomDates } from '../room-dates.model';
+
 @Component({
   selector: 'app-date-selector',
   templateUrl: './date-selector.component.html',
   styleUrls: ['./date-selector.component.sass'],
 })
 export class DateSelectorComponent implements OnInit {
-  constructor() {}
+  constructor(private makeReservationService: MakeReservationService) {}
+
+  private currentRoomDates: RoomDates = {};
 
   ngOnInit(): void {
-    return;
+    const selectedRoom = this.makeReservationService.getSelectedRoom();
+    this.currentRoomDates = this.makeReservationService.getAvailableDatesForRoom(selectedRoom);
   }
+
+  getAvailableDatesForRoom = (d: Date | null): boolean => {
+    const date = d || new Date();
+    const year = this.currentRoomDates[date.getFullYear()];
+    const month = year ? year[date.getMonth() + 1] : false;
+    const day = month ? month[date.getDate()] : false;
+
+    return day !== false && day !== undefined;
+  };
 }
